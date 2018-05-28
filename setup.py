e56cbebb3e0333ed79ac39249fb16e6a1f3e528d
Andrew Barisser <barisser@protonmail.com>
2018-05-28 01:36:35 -0700

    minor setup.py changes

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup
 
 setup(
-      name='pyproof',
-      version='0.0.1',
-      description='Solid as a Rock',
-      license='MIT',
-      install_requires=[
-          'pycrypto'
+    name='pyproof',
+    author='Andrew Barisser',
+    author_email='barisser@protonmail.com',
+    version='0.0.1',
+    url='https://github.com/barisser/pyproof',
+    description='Solid as a Rock',
+    license='MIT',
+    install_requires=[
+        'pycrypto'
         ],
-      packages=['pyproof'],
-      tests_require=[
-          'numpy',
-          'pytest',
-          'pytest-cov',
-          'sympy'
+    packages=['pyproof'],
+    tests_require=[
+        'numpy',
+        'pytest',
+        'pytest-cov',
         ]
       )
